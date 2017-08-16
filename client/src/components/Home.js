497319b053defe3e5295db53224b5e4dfe7ad735
anzcarroll <anzcarroll@gmail.com>
2017-08-16 14:30:27 -0400

    tried to add a little bit more styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 const ResponseStyle = styled.div`
 display: flex;
-justify-content: center;
+text-align: center;
 .title-question: {
   position: relative;
   display: inline-block;
@@ -25,13 +25,21 @@ justify-content: center;
   padding: 12px 16px;
   display: flex;
   justify-content: center;
+  text-align: center;
 }
 h1, Link, a {
   font-family: 'BenchNine', sans-serif;
+  text-align: center;
   font-weight: 700;
   font-size: 2rem;
 }
+`
 
+const ViewHomeStyle = styled.div`
+text-align: center;
+font-family: 'BenchNine', sans-serif;
+font-weight: 700;
+font-size: 2rem;
 `
 
 
@@ -66,6 +74,17 @@ class Home extends Component {
             </div>
           </ResponseStyle>
         </div>
+        <hr/>
+        <ViewHomeStyle>
+        <p>Some Days are harder than others! <br/>
+        Seasons change, good books end, its Monday and <br/>
+        your boss is still an asshole, <br/>
+        Revolution was out of Orange Pistachio? <br/>
+        Who knows! But when you click here you can <br/>
+        get a chuckle or a smile. <br/>
+        Only Rule is for you to promise to:<br/>
+        shed no tears today + <br/>
+        express your true brave self today</p>
         <h1>View Inpirational boards created by others:</h1>
         
         {this.state.board.map((board, i) => (
@@ -76,6 +95,7 @@ class Home extends Component {
             <button>Delete</button>
           </div>
         ))}
+        </ViewHomeStyle>
       </div>
     );
   }
